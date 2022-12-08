67e4efa3527d57467c09c5904a4e67c43c9c0833
Nafis Khan <nafisrizwank@gmail.com>
2022-12-08 17:17:57 -0500

    implemented categories and product page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,12 +13,9 @@ const Footer = () => {
       <div className='max-w-[1240px] mx-auto'>
         <div className='flex items-center justify-around p-2 mobile:flex-col mobile:items-start'>
           <div className='flex-1 flex flex-col flex-wrap p-2'>
-            <h1 className='text-[25px] text-white'>ZAINKEEPSCODE</h1>
+            <h1 className='text-[25px] text-white'>Rocket Kiks.</h1>
             <p className=' text-white'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Voluptatem soluta quis porro eveniet. Nulla modi, quas unde eaque
-              vel, quaerat repellendus dignissimos iure itaque architecto
-              pariatur quia a eveniet veniam?
+              Info about the company, its history, etc.
             </p>
             <div className='flex items-center justify-center mt-3 self-start'>
               <div className={socialStyle + ` bg-blue-700`}>
@@ -36,15 +33,15 @@ const Footer = () => {
           <div className='flex-1 flex flex-col p-2'>
             <div className='flex m-3'>
               <LocationOnIcon className='text-[#8a4af3]' />
-              <p className='pl-3 text-white'>State of California</p>
+              <p className='pl-3 text-white'>New York</p>
             </div>
             <div className='flex m-3'>
               <LocalPhoneIcon className='text-[#521da8]' />
-              <p className='pl-3 text-white'>+92 12345678</p>
+              <p className='pl-3 text-white'>+1800 626 997</p>
             </div>
             <div className='flex m-3'>
               <EmailOutlinedIcon className='text-[#8a4af3]' />
-              <p className='pl-3  text-white'>Ahmadroa925@gmail.com</p>
+              <p className='pl-3  text-white'>rocketkiks@gmail.com</p>
             </div>
           </div>
         </div>
